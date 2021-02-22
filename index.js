a99785f27d59ce6842df59fdfd120fdc8acde687
Jay Wagnon <jaywagnon@gmail.com>
2021-02-22 14:35:50 -0700

    Add cors and some refactor.

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var app = express();
+app.set('trust proxy', true)
+
+const cors = require('cors')
+app.use(cors())
 
 app.set('port', (process.env.PORT || 9000));
 
 app.get('/ping/v1', (req, res) => {
+  console.log(req)
   res.status(200).json({ ping: 'pong' })
-});
+})
 
 app.all('*', (req, res) => {
   res.status(503).json({ error: 'Not implemented.'})
-});
+})
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'))
-});
+const PORT = app.get('port')
+app.listen(PORT, () => {
+  console.log('Node app is running on port', PORT)
+})
