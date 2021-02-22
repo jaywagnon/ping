0affba3080c640ed86ed9f62878706e33002f00e
Jay Wagnon <jaywagnon@gmail.com>
2021-02-22 14:30:22 -0700

    Update packaging and add catch-al.

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 9000));
 
-app.get('/ping/v1', function onRequest(request, response) {
-  response.send({ ping: 'pong' });
+app.get('/ping/v1', (req, res) => {
+  res.status(200).json({ ping: 'pong' })
+});
+
+app.all('*', (req, res) => {
+  res.status(503).json({ error: 'Not implemented.'})
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port'))
 });
